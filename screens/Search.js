b074168c04feb6e5c849d10d8f0296d44c01e013
Fadlian17 <alfansyahfadlian@gmail.com>
2020-05-12 22:44:22 +0700

    navigation tabs

--- screens/Search.js
+++ screens/Search.js
@@ -3,7 +3,7 @@ import { TextInput,Button,Card } from 'react-native-paper';
 import {View,Text, FlatList} from 'react-native'
 import Header from './Header'
 
-export default Search =()=> {
+export default Search =({navigation})=> {
 
     //deskripsi variabel
     const [city,setCity] = useState('')
@@ -17,6 +17,16 @@ export default Search =()=> {
         })
     }
 
+    //navigasi data 
+    const btnClick = ()=>{
+        navigation.navigate("home",{city:city})
+    }
+
+    const listClick =(cityname)=>{
+        setCity(cityname)
+        navigation.navigate("home",{city:cityname})
+    }
+
     return (
         <View style={{flex:1}}>
             <Header name="Search Screen"/>
@@ -27,7 +37,7 @@ export default Search =()=> {
                 onChangeText={(text)=>fetchCities(text)}
             />
 
-        <Button icon="content-save" mode="contained" theme={{colors:{primary:"#00aaff"}}} style={{margin:20}} onPress={() => console.log('Pressed')}>
+        <Button icon="content-save" mode="contained" theme={{colors:{primary:"#00aaff"}}} style={{margin:20}} onPress={() => btnClick()}>
             <Text style={{color:"white"}}>Press me</Text>
         </Button>
 
@@ -35,7 +45,7 @@ export default Search =()=> {
             data={cities}
             renderItem={({item})=>{
                 return(
-                    <Card style={{margin:2,padding:12}}>
+                    <Card style={{margin:2,padding:12}} onPress={()=>listClick(item.name)}>
                         <Text>{item.name}</Text>
                     </Card>
                 )
